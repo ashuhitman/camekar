eeddc3cc5033fd774c089d1bf82f1dfa68ff46ab
ashuhitman <ashu8306@gmail.com>
2019-08-27 01:30:17 +0530

    Updates

---
+++ precache-manifest.fab5e911c4d9f5ab8c63e42550131ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ed851a752d950626d87c5fa7a36dd5",
+    "url": "/camekar/index.html"
+  },
+  {
+    "revision": "3a5f229ea871613d5967",
+    "url": "/camekar/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6983e0042a6e89811b67",
+    "url": "/camekar/static/js/2.c281ee8c.chunk.js"
+  },
+  {
+    "revision": "3a5f229ea871613d5967",
+    "url": "/camekar/static/js/main.fa084407.chunk.js"
+  },
+  {
+    "revision": "63e0b25ca9bb0bd79705",
+    "url": "/camekar/static/js/runtime~main.5affbd02.js"
+  },
+  {
+    "revision": "654a7c8eb657ea478032db30272fdc36",
+    "url": "/camekar/static/media/boat.654a7c8e.jpg"
+  }
+]);
